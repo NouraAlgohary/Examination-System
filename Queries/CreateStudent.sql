fb0c3e2a5a536438229e8c4ef712b9eb796e7de4
Noura Algohary <103903785+NouraAlgohary@users.noreply.github.com>
2024-02-07 11:40:11 +0200

    Add files via upload

---
+++ Queries/CreateStudent.sql
@@ -0,0 +1,30 @@
+USE [ExaminationSystemDB]
+GO
+
+/****** Object:  Table [dbo].[Student]    Script Date: 2/7/2024 11:38:02 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Student](
+	[StudentID] [int] NOT NULL,
+	[FirstName] [varchar](20) NOT NULL,
+	[LastName] [varchar](20) NULL,
+	[Age] [int] NULL CHECK ([Age] BETWEEN 20 AND 35),  -- Check constraint for Age
+	[Gender] [varchar](6) NULL,
+	[Phone] [nchar](11) NULL,
+	[Location] [varchar](50) NULL,
+	[Faculty] [varchar](50) NULL,
+	[GraduationYear] [int] NULL,
+	[Email] [varchar](50) NULL,
+	[Password] [varchar](50) NULL,
+ CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED 
+(
+	[StudentID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
