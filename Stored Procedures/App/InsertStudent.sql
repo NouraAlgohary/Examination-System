bbe53733a3e93a03dc82ed9790df7f116a51f780
Noura Algohary <noura.malgohary@gmail.com>
2024-02-12 01:42:34 +0200

    add stored procedures

---
+++ Stored Procedures/App/InsertStudent.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE InsertStudent
+    @firstname VARCHAR(50), @lastname VARCHAR(50), @age int, @gender VARCHAR(6), @phone NCHAR(10), 
+	@location VARCHAR(255), @faculty VARCHAR(50), @gradutaionyear INT, @Email VARCHAR(50), @Password VARCHAR(50), 
+	@trackname VARCHAR(50), @branchname VARCHAR(50)
+AS
+BEGIN
+	DECLARE @trackid INT 
+	SELECT @trackid = TrackID FROM Track WHERE TrackName = @trackname
+
+    INSERT INTO Student (FirstName, LastName, Age, Gender, Phone, [Location], Faculty, GraduationYear, Email, [Password], TrackID)
+    VALUES (@firstname, @lastname, @age, @gender, @phone, @location, @faculty, @gradutaionyear,@email, @Password, @trackid)
+END;
+
+EXEC InsertStudent @firstname = "Noura", @lastname = "Mahmoud", @age=24, @gender="Female", 
+     @phone = "1092837464", @location = "Mansoura", @faculty ="Engineering", @gradutaionyear = 2023, 
+	 @email = "noura@gmail.com", @Password = "a1b2c3d4e5", @trackname="Power BI", @branchname="Mansoura"
