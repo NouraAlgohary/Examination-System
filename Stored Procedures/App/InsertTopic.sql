c7be38c17952e4b19b4ce0b0fdfe0f105973db94
Noura Algohary <noura.malgohary@gmail.com>
2024-02-13 22:39:07 +0200

    update stored procedures

---
+++ Stored Procedures/App/InsertTopic.sql
@@ -0,0 +1,17 @@
+ALTER PROCEDURE InsertTopic 
+    @topicid INT,  -- assuming @topicid is provided
+    @topicname VARCHAR(50)
+AS
+BEGIN
+    -- Enable identity insert for the Topic table
+    SET IDENTITY_INSERT Topic ON;
+
+    -- Insert a new topic into the Topic table with the provided TopicID
+    INSERT INTO Topic (TopicID, TopicName)
+    VALUES (@topicid, @topicname);
+
+    -- Disable identity insert after the operation
+    SET IDENTITY_INSERT Topic OFF;
+END;
+
+EXEC InsertTopic @topicname = 'NewTopic';
