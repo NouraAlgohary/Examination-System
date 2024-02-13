0406a5ea16a2d3592e6f7e36449c617f0bb8d57c
MohamedEssam52 <50272760+MohamedEssam52@users.noreply.github.com>
2024-02-13 22:28:33 +0200

    Update InsertStudent.sql
    Added missed parameter in the insert statement

--- Stored Procedures/App/InsertStudent.sql
+++ Stored Procedures/App/InsertStudent.sql
@@ -1,14 +1,18 @@
 CREATE PROCEDURE InsertStudent
     @firstname VARCHAR(50), @lastname VARCHAR(50), @age int, @gender VARCHAR(6), @phone NCHAR(10), 
 	@location VARCHAR(255), @faculty VARCHAR(50), @gradutaionyear INT, @Email VARCHAR(50), @Password VARCHAR(50), 
-	@trackname VARCHAR(50), @branchname VARCHAR(50)
+	@branchname VARCHAR(50),@trackname VARCHAR(50)
 AS
 BEGIN
 	DECLARE @trackid INT 
 	SELECT @trackid = TrackID FROM Track WHERE TrackName = @trackname
 
-    INSERT INTO Student (FirstName, LastName, Age, Gender, Phone, [Location], Faculty, GraduationYear, Email, [Password], TrackID)
-    VALUES (@firstname, @lastname, @age, @gender, @phone, @location, @faculty, @gradutaionyear,@email, @Password, @trackid)
+	DEClARE @BranchID INT 
+	SELECT @BranchID = BranchID FROM Branch WHERE BranchLocation = @branchname;
+
+
+    INSERT INTO Student (FirstName, LastName, Age, Gender, Phone, [Location], Faculty, GraduationYear, Email, [Password],BranchID, TrackID)
+    VALUES (@firstname, @lastname, @age, @gender, @phone, @location, @faculty, @gradutaionyear,@email, @Password,@BranchID ,@trackid)
 END;
 
 EXEC InsertStudent @firstname = "Noura", @lastname = "Mahmoud", @age=24, @gender="Female", 
