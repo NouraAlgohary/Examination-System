1c2235d946abbf7e80e95a78814ac692b81891d9
MohamedEssam52 <50272760+MohamedEssam52@users.noreply.github.com>
2024-02-16 18:36:23 +0200

    Update InsertCourse.sql to insert the track which is the course assigned to
    added the option to assign the course to its given track  it in the trackcourse table

--- Stored Procedures/App/InsertCourse.sql
+++ Stored Procedures/App/InsertCourse.sql
@@ -1,5 +1,7 @@
+----This procedure will insert a new course -- track name must be given to be assigned to it in the trackcourse
+
 CREATE PROCEDURE InsertCourse 
-				@coursename VARCHAR(255), @courseduration INT, @instructorname VARCHAR(50)
+				@coursename VARCHAR(255), @courseduration INT, @instructorname VARCHAR(50), @TrackName VARCHAR(50)
 AS 
 BEGIN
 	DECLARE @instructorid INT
@@ -9,6 +11,15 @@ BEGIN
 
     INSERT INTO Course (CourseName, CourseDuration, InstructorID)
 	VALUES (@coursename, @courseduration, @instructorid)
-END;
+	
+	DECLARE @COURSEID INT
+	SET @COURSEID =SCOPE_IDENTITY()
+	
+	Declare @TrackID INT
+	SELECT @TrackID = TrackID FROM Track 
+	WHERE TrackName = @TrackName
 
-EXEC InsertCourse @coursename = newCourse, @courseduration=30, @instructorname="Samira Saleh"
+
+
+	INSERT INTO TrackCourse(TrackID,CourseID) VALUES(@TrackID,@COURSEID)
+END;
